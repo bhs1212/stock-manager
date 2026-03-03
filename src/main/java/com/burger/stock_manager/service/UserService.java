84afb7e7b74997d3801a911eb96656de79021634
bhs1212 <hasun17kh@gmail.com>
2026-03-03 17:31:44 +0900

    chore: Spring Security 전환 후 불필요한 코드 제거
    - LoginCheckInterceptor, AdminCheckInterceptor 삭제
    - UserService.authenticate 메서드 삭제
    - UserSessionDTO 삭제

--- src/main/java/com/burger/stock_manager/service/UserService.java
+++ src/main/java/com/burger/stock_manager/service/UserService.java
@@ -2,7 +2,6 @@ package com.burger.stock_manager.service;
 
 import com.burger.stock_manager.mapper.UserMapper;
 import com.burger.stock_manager.model.UserDTO;
-import com.burger.stock_manager.model.UserSessionDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,21 +16,6 @@ public class UserService {
 
     @Autowired
     private BCryptPasswordEncoder encoder;
-
-    // 로그인 검증 로직 (아이디/비밀번호 확인)
-    public UserSessionDTO authenticate(String username, String password) {
-        UserDTO user = userMapper.findByUsername(username);
-
-        if (user != null && encoder.matches(password, user.getPassword())) {
-            UserSessionDTO sessionUser = new UserSessionDTO();
-            sessionUser.setId(user.getId());
-            sessionUser.setUsername(user.getUsername());
-            sessionUser.setName(user.getName());
-            sessionUser.setRole(user.getRole());
-            return sessionUser;
-        }
-        return null;
-    }
 
     // 아이디 중복 체크 로직
     public boolean isUsernameTaken(String username) {
